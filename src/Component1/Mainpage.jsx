eab0514d98b1ba1e9d9a3805c5ee945a1df81f9b
Manoj M S <163495384+Manojms2004@users.noreply.github.com>
2026-03-07 19:28:30 +0530

    Landing page text added

--- src/Component1/Mainpage.jsx
+++ src/Component1/Mainpage.jsx
@@ -5,7 +5,7 @@ function Mainpage() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-screen relative">
+    <div className="w-full h-[100vh] relative overflow-hidden">
       <video autoPlay muted loop className="absolute w-full h-full object-cover z-[-1] inset-0"
       >
         <source
@@ -20,17 +20,37 @@ function Mainpage() {
         </p>
       </div>
 
-      <div className="flex space-x-4 justify-center mt-80">
-        <button
-          type="button"
-          className="px-10 py-2 bg-transparent border-3 border-black text-2xl text-black rounded-md hover:bg-white hover:text-black transition cursor-pointer"
-          onClick={() =>{
-            
-           navigate('/login');}} > Login </button>
-        <button type="button" className="px-10 py-2 bg-transparent border-3 border-black text-2xl text-black rounded-md hover:bg-white hover:text-black transition cursor-pointer"
-          onClick={() => {
-           
-            navigate('/signup');}}>Signup </button>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+
+        <div className="text-center mb-10">
+          <p className="text-5xl font-bold">
+            TrendyThreads
+            Wear Your Style with Confidence
+          </p>
+        </div>
+
+        <div className="flex space-x-10 mb-20">
+          <button
+            type="button"
+            className="px-10 py-2 text-2xl cursor-pointer font-semibold text-black border-2 border-black rounded-xl bg-transparent backdrop-blur-sm transition-all duration-300 hover:bg-black hover:text-white hover:scale-105 hover:shadow-xl"
+            onClick={() => {
+              navigate('/login');
+            }}
+          >
+            Login
+          </button>
+
+          <button
+            type="button"
+            className="px-10 py-2 text-2xl cursor-pointer font-semibold text-black border-2 border-black rounded-xl bg-transparent backdrop-blur-sm transition-all duration-300 hover:bg-black hover:text-white hover:scale-105 hover:shadow-xl"
+            onClick={() => {
+              navigate('/signup');
+            }}
+          >
+            Signup
+          </button>
+        </div>
+
       </div>
     </div>
   );
